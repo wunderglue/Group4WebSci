2b41ed1379662ce4117890c8bb357f99395d04bf
JonathanWong592 <jonathanwong592@gmail.com>
2020-02-11 15:54:32 -0500

    Boilerplate

---
+++ README.md
@@ -0,0 +1,7 @@
+Steps to run
+1. Install node.js
+2. Create new directory for app
+3. Place all files in directory and cd into it
+4. In command line go to directory then: npm install
+5. In command line do: npm start
+6. In browser go to localhost:3000
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+
+const router = require('./routers/router')
+app.use(router)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Running on port ${port}`))
