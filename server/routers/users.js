fd9a53f8b54bbecc3caac814312612921b191807
JonathanWong592 <jonathanwong592@gmail.com>
2020-03-04 19:58:54 -0500

    Resolved issues

--- server/routers/users.js
+++ server/routers/users.js
@@ -14,43 +14,6 @@ router.get('/me', function (req, res) {
     })
 })
 
-// Test adding information to DB
-router.get('/testDB', async (req, res) => {
-    console.log("Connected testDB")
-    // test results
-    testResults = {
-        "exercise": "Squat",
-        "repititions": "10",
-        "weight": "185"
-    }
-    await Practice.findOneAndUpdate({rcsid: "testy1"}, testResults, (err, res) => {
-        if (err) return res.send(500, {error: err});
-        return res.send('Succesfully saved.');
-    })
-
-    // if user doesn't exist, create a new user.
-    // Student, league, practice_type, results: (exercise, repitition, weight)
-    testPractice = {
-        "student": "test",
-        "rcsid": "testy1",
-        "league": "1",
-        "practice_type": "Lift",
-        "results": [{
-            "exercise": "Bench",
-            "repititions": "5",
-            "weight": "155"
-        }]
-    }
-    const prac = new Practice(testPractice)
-    console.log(prac)
-    prac.save(function(err, prac) {
-        console.log("Saved")
-        console.log(err, prac)
-        return res.json({
-            username: "Returned"
-        })
-    })
-})
 
 router.get('/aggregateStats', async (req, res) => {
     console.log("Aggregate")
