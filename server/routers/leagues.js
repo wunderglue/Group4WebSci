210d8b12c0a3e19c0c002995e23afce7e9e54fb1
Fred Buchanan <oscillator.b@gmail.com>
2020-04-16 13:47:29 -0400

    Added coach creation and deletion, and question deletion.

--- server/routers/leagues.js
+++ server/routers/leagues.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const moongoose = require('mongoose')
 const router = express.Router()
 const auth = require('../authentication')
 const League = require('../models/league')
@@ -7,6 +8,17 @@ const League = require('../models/league')
 router.use(auth.block)      // IF YOU ARE ADDING ANOTHER ROUTES FILE, YOU PROBABLY WANT THIS LINE!!!!!
 
 async function getLeagueOr404(id, res) {
+    try {
+        id = moongoose.Types.ObjectId(id)
+    } catch(e) {
+        res.statusCode = 400
+        res.json({
+            'error':'validation',
+            'message':`${id} is not a valid id`
+        })
+        throw e
+    }
+
     const league = await League.findOne({_id: id})
 
     // Error if the league you are trying to edit does not exist
@@ -44,12 +56,17 @@ async function saveAndValidate(league, res) {
     }
 }
 
-// Special /me route returns information private to the user
+/**
+ * Fetch a league
+ */
 router.get('/', async function (req, res) {
     const leagues = await League.find()
     res.json(leagues.map(l => l.toObject()))
 })
 
+/**
+ * Create a league
+ */
 router.post('/', async function (req, res) {
     const league = new League(req.body)
     saveAndValidate(league, res)
@@ -57,16 +74,24 @@ router.post('/', async function (req, res) {
 
 
 /**
+ * Get a single league by ID
+ */
+router.get('/:id', async function (req, res) {
+    const league = await getLeagueOr404(req.params.id, res)
+    res.json(league.toObject())
+})
+
+/**
  * Add a member to the league.
  */
-router.put('/:id/members/:member', async function (req, res) {
+router.put('/:id/members/:rcs_id', async function (req, res) {
     const user = await auth.getUser(req)
 
     const league = await getLeagueOr404(req.params.id, res)
     ensureCoach(league, user, res)
 
     // Do the actual logic
-    league.members.push(req.params.member)
+    league.members.push(req.params.rcs_id)
 
     // Save the league and error if validation fails
     await saveAndValidate(league, res)
@@ -75,14 +100,46 @@ router.put('/:id/members/:member', async function (req, res) {
 /**
  * Remove member from the league
  */
-router.delete('/:id/members/:member', async function (req, res) {
+router.delete('/:id/members/:rcs_id', async function (req, res) {
     const user = await auth.getUser(req)
 
     const league = await getLeagueOr404(req.params.id, res)
     ensureCoach(league, user, res)
 
     // Do the actual logic
-    league.members = league.members.filter(m => m !== req.params.member)
+    league.members = league.members.filter(m => m !== req.params.rcs_id)
+
+    // Save the league and error if validation fails
+    await saveAndValidate(league, res)
+})
+
+/**
+ * Add a coach to the league.
+ */
+router.put('/:id/coaches/:rcs_id', async function (req, res) {
+    const user = await auth.getUser(req)
+
+    const league = await getLeagueOr404(req.params.id, res)
+    ensureCoach(league, user, res)
+
+    // Do the actual logic
+    league.coaches.push(req.params.rcs_id)
+
+    // Save the league and error if validation fails
+    await saveAndValidate(league, res)
+})
+
+/**
+ * Remove member from the league
+ */
+router.delete('/:id/coaches/:rcs_id', async function (req, res) {
+    const user = await auth.getUser(req)
+
+    const league = await getLeagueOr404(req.params.id, res)
+    ensureCoach(league, user, res)
+
+    // Do the actual logic
+    league.coaches = league.coaches.filter(c => c !== req.params.rcs_id)
 
     // Save the league and error if validation fails
     await saveAndValidate(league, res)
@@ -103,5 +160,23 @@ router.post('/:id/questions', async function (req, res) {
     // Save the league and error if validation fails
     await saveAndValidate(league, res)
 })
+
+/**
+ * Delete a question by id
+ */
+
+router.delete('/:league_id/questions/:question_id', async function (req, res) {
+    const user = await auth.getUser(req)
+
+    const league = await getLeagueOr404(req.params.league_id, res)
+    ensureCoach(league, user, res)
+
+    // Do the actual logic
+    league.questions = league.questions.filter(q => q._id != req.params.question_id)
+
+    // Save the league and error if validation fails
+    await saveAndValidate(league, res)
+})
+
 
 module.exports = router
