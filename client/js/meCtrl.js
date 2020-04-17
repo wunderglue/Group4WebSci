6cfa9f2ed6b6de42092f9eb75fb1c1acd04b5cbc
Fred Buchanan <oscillator.b@gmail.com>
2020-04-17 11:28:45 -0400

    Improved per user statistics view.

--- client/js/meCtrl.js
+++ client/js/meCtrl.js
@@ -1,21 +1,15 @@
-app.controller('meCtrl', function ($scope, userService) {
-    userService.getMyStatistics().then(function (statistics) {
-        $scope.$apply(function() {
-            $scope.statistics = statistics
+app.controller('meCtrl', function($scope, $http, userService) {
+    userService.onUserStateChange( function (user) {
+        console.log("HERE!")
+        const username = user.rcs_id
+        $http.get(`/api/users/${username}/statistics`).then((resp) => {
+            console.log(resp.data)
+            $scope.statistics = resp.data
+            // console.log(resp.data[0].keys)
+            keys = (Object.keys(resp.data[0]))
+            $scope.keys = keys
+            // $scope.response = resp.data
+            return resp.data
         })
     })
-})
-
-
-app.controller('meCtrl', function($scope, $http) {
-    const username = "123456"
-    $http.get(`/api/users/${username}/statistics`).then((resp) => {
-        console.log(resp.data)
-        $scope.statistics = resp.data
-        // console.log(resp.data[0].keys)
-        keys = (Object.keys(resp.data[0]))
-        $scope.keys = keys
-        // $scope.response = resp.data
-        return resp.data
-    }) 
 });
--- client/js/userService.js
+++ client/js/userService.js
@@ -12,6 +12,7 @@ app.service('userService', function ($http, $route) {
 
     // register a function to be called when the user state change (for instance, the username is fetched from the server)
     this.onUserStateChange = function (callback) {
+        console.log(callback)
         _userStateChangeCallbacks.push(callback)
         callback(_user)
     }
@@ -22,20 +23,9 @@ app.service('userService', function ($http, $route) {
         window.location = "/api/users/me/logout"
     }
 
-    this.getMyStatistics = async function() {
-
-        const username = "123456"
-        await $http.get(`/api/users/${username}/statistics`).then((resp) => {
-            console.log(resp.data)
-            // $scope.response = resp.data
-            return resp.data
-        }) 
-
-    }
-
     $http.get('/api/users/me')
         .then(function (resp) {
             updateUser(resp.data)
-            $route.reload()
+            // $route.reload()
         })
 })
